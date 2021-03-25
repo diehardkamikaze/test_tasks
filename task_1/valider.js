25a786cc35d9402a3490d5c9d7deb66382bfec71
diehardkamikaze <diehardkamikaze@gmail.com>
2021-03-25 16:21:31 +0300

    added []

--- task_1/valider.js
+++ task_1/valider.js
@@ -30,57 +30,70 @@ function handle_key(data, obj)
 
 function handle_object(data, obj)
 {
+    var flag;
     skip_spaces(data, obj);
 
     if (data[obj.i] == '}' && ++obj.i)
         return (1);
-    if (!handle_key(data, obj))
-        return (0);
-    if (data[obj.i] !== ':')
-        return (0);
-
-    ++obj.i;
-    if (!json_parser(data, obj))
-        return (0);
-    while (data[obj.i] == ',')
+    flag = 1;
+    while (flag)
     {
-        obj.i++;
         skip_spaces(data, obj);
         if (!handle_key(data, obj))
             return (0);
         if (data[obj.i] !== ':')
             return (0);
         ++obj.i;
-
         if (!json_parser(data, obj))
             return (0);
-
+        if (data[obj.i] == ',')
+            obj.i++;
+        else flag = 0;
     }
     if (data[obj.i] == '}' && ++obj.i)
         return (1);
     return (0);
 }
 
+function handle_array(data, obj)
+{
+    var flag;
+    skip_spaces(data, obj);
+    
+    if (data[obj.i] == ']' && ++obj.i)
+        return (1);
+    flag = 1;
+    while (flag)
+    {
+        if (!json_parser(data, obj))
+            return (0);
+        if (data[obj.i] == ',')
+            obj.i++;
+        else flag = 0;
+    }
+    skip_spaces(data, obj);
+    if (data[obj.i] == ']' && ++obj.i)
+        return (1);
+    return (0);
+}
+
 function json_parser(data, obj)
 {
+    var result = 0;
     skip_spaces(data, obj);
+
     if (data[obj.i] === undefined)
-        return (1);
-
-    if (data[obj.i] == '{' && ++obj.i)
-    {
-        if(!handle_object(data, obj))
-            return (0);
-    } 
+        result = 1;
+    else if (data[obj.i] == '{' && ++obj.i)
+        result = handle_object(data, obj);
     else if (data[obj.i] == '"' && ++obj.i)
-    {
-        if (!handle_string(data, obj))
-            return (0);
-    }
-    else
+        result = handle_string(data, obj);
+    else if (data[obj.i] == "[" && ++obj.i)
+        result = handle_array(data, obj);
+    if (!result)
         return (0);
-    skip_spaces(data, obj);
 
+    skip_spaces(data, obj);
     return (1);
 }
 
@@ -90,39 +103,10 @@ function json_valider(event)
     var obj = {};
 
     obj.i = 0;
-   // json_parser(data, obj);
     if (json_parser(data, obj) && data[obj.i] == undefined)
         result.innerHTML = "VALID";
     else
         result.innerHTML = "INVALID";
-        //else result.innerHTML = "INVALID!";
-    /* var i;
-
-    i = 0;
-    while (i < data.length)
-    {
-        while (is_space(data[i]))
-            i++;
-        if (data[i] != '{')
-            return (0);
-        i++;
-        while (is_space(data[i]))
-            i++;
-        if (data[i] != '"')
-            return (0);
-        i++;
-        while (is_alpha(data[i]))
-            i++;
-        if (data[i] != '"')
-            return (0);
-        i++;
-        while (is_space(data[i])
-            i++;
-        if (data[i] != ':')
-            return (0);
-
-    }
-    return (1); */
  
 }
 
