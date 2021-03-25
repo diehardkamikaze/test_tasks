43d327e8ef8ed862156f60282a9e249f8ee447e5
diehardkamikaze <diehardkamikaze@gmail.com>
2021-03-25 15:19:20 +0300

    added 2 task

---
+++ task_2/i_have_no_idea.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const input = "input.csv";
+const output = "output.csv"
+var csv = fs.readFileSync('input.csv', 'utf8').split('\n');
+
+var final = [];
+var result = {};
+
+var i = 1;
+var tmp;
+
+
+while (i < csv.length)
+{
+    tmp = csv[i].split(',');
+    if(tmp[2][0] === 'н' && i++)
+        continue;
+    if (!result[tmp[0]])
+        result[tmp[0]] = {};
+    result[tmp[0]][parseInt(tmp[1])] = i;
+    i++;
+}
+
+for(street in result)
+    for(num in result[street])
+        if (result[street][num])
+            final.push(csv[result[street][num]]);
+
+
+fs.writeFileSync(output, final.join('\n'));
